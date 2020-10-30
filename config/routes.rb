c452b487f4d753389fd8f6ddc4ff7157e28110d0
choco-chai <umvienta@gmail.com>
2020-10-30 10:51:20 +0900

    showページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'schedules/index'
   devise_for :users
   root to: "schedules#index"
-  resources :schedules, only: [:index, :new]
+  resources :schedules, only: [:index, :new, :show]
 end
