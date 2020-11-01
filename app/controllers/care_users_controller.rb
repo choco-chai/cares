8244bf25c7011a4cd43af2e1e844aceaae9236a0
choco-chai <umvienta@gmail.com>
2020-11-01 22:04:21 +0900

    care_users createアクション設定

--- app/controllers/care_users_controller.rb
+++ app/controllers/care_users_controller.rb
@@ -2,4 +2,19 @@ class CareUsersController < ApplicationController
   def new
     @care_user = CareUser.new
   end
+
+  def create
+    @care_user = CareUser.new(care_user_params)
+    if @care_user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def care_user_params
+    params.require(:care_user).permit(:name)
+  end
 end
