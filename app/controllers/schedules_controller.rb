ce7582a018211018a182445eb8e7225327b45878
choco-chai <umvienta@gmail.com>
2020-11-03 14:31:34 +0900

    ケア内容新規投稿機能編集

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -6,10 +6,19 @@ class SchedulesController < ApplicationController
     @care_users = CareUser.all
   end
 
+  def new
+    @care_user = CareUser.find(params[:care_user_id])
+    @schedule = Schedule.new
+  end
+
   def create
     @care_user = CareUser.find(params[:care_user_id])
     @schedule = @care_user.schedules.new(schedule_params)
-    @schedule.save
+    if @schedule.save
+      redirect_to care_user_schedules_path(@care_user)
+    else
+      render :index
+    end
   end
 
   def show
@@ -18,6 +27,6 @@ class SchedulesController < ApplicationController
   private
 
   def schedule_params
-    params.require(:schedule).permit(:times, :title, :text)
+    params.require(:schedule).permit(:times, :title, :text).merge(care_user_id: params[:care_user_id])
   end
 end
