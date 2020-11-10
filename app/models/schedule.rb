d8b144ed04dc69a4b54033df6ef382bab7418648
choco-chai <umvienta@gmail.com>
2020-11-10 15:15:25 +0900

    エラーメッセージ表示を日本語に変更

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,6 @@
 class Schedule < ApplicationRecord
   with_options presence: true do
-    validates :times, format: { with: /\A([01][0-9]|2[0-3]):[0-5][0-9]\Z/ , message: 'の入力には:を使用してください(例 13:30)'}
+    validates :times, format: { with: /\A([01][0-9]|2[0-3]):[0-5][0-9]\Z/ , message: 'の入力には:を使用してください。1~9時の場合は先頭に0を入れてください(例 09:30,13:00)'}
     validates :title, :text
   end
 
